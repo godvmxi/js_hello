22e8f5e03e6c814dd92b030af74661b8b67f008e
Bright Jiang <bright.jiang@amd.com>
2020-08-17 13:16:54 +0800

    add js run in blowser

---
+++ puppeteer/get-dimensions.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch(
+    {
+        devtools: true,
+        slowMo: 250, //slow down by 250ms
+        headless: false
+    }
+  );
+  const page = await browser.newPage();
+  await page.goto('https://baidu.com');
+
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+
+  await browser.close();
+})();
